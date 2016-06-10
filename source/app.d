44e058402c12f57aa57da3d1651de48e38b9763a
Zane Sterling <zsterling@google.com>
2016-06-10 15:27:29 -0700

    Add num-lines flag

--- source/app.d
+++ source/app.d
@@ -1,22 +1,32 @@
+import std.algorithm.iteration;
 import std.conv;
+import std.getopt;
 import std.range;
 import std.range.primitives;
 import std.stdio;
 
 int main(string[] args) {
-	if (args.length != 2) {
-		usage(args[0]);
+	size_t numLines = 1;
+	auto helpInfo = getopt(
+		args,
+		"n", "Number of lines to pick", &numLines,
+	);
+
+	if (args.length != 2 || numLines < 0) {
+		usage(args[0], helpInfo);
 		return 1;
 	}
 
-	size_t line_number = to!size_t(args[1]);
+	size_t lineNumber = to!size_t(args[1]);
 	stdin.byLine
-		.drop(line_number - 1)
-		.front
-		.writeln();
+		.drop(lineNumber - 1)
+		.take(numLines)
+		.each!writeln;
 	return 0;
 }
 
-void usage(string name) {
-	writefln("usage: %s line_number", name);
+void usage(string name, GetoptResult helpInfo) {
+	writefln("usage: %s [flags] lineNumber", name);
+	writeln();
+	defaultGetoptPrinter("Flags:", helpInfo.options);
 }
